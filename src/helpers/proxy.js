74865c4b169d44811e44d0cc63944abc511d4c33
liujunjie <liujunjie@bbdservice.com>
2019-06-19 21:59:09 +0800

    修复//开头的baseURL不能代理的bug

--- src/helpers/proxy.js
+++ src/helpers/proxy.js
@@ -1,11 +1,13 @@
 import { isBlank } from 'helpers/util';
 
 // 根据 baseURL 生成代理拦截的 url
-export function proxyBaseURL(baseURL) {
-    var _baseURL = baseURL;
+export function proxyBaseURL(baseURL = '') {
+    var _baseURL = baseURL.trim();
 
     // 如果为空则使用当前服务的 host
-    if (isBlank(_baseURL)) {
+    if (_baseURL.startsWith('//')) {
+        _baseURL = location.protocol + _baseURL;
+    } else if (isBlank(_baseURL)) {
         _baseURL = `${location.protocol}//${location.host}`;
     }
     // 截取开始和末尾的 '/'
